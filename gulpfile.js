8d36c5329f2c590161e02c8738c36e970b59b84f
Mike Zevitas <mzevitas@gmail.com>
2015-06-09 09:09:47 -0400

    updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,5 @@
 'use strict';
-// generated on 2015-04-01 using generator-tiy-webapp 0.0.11
 
-// Require your modules
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 var rimraf = require('rimraf');
